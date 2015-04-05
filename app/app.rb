abb245acb697e8bc81640952527c3b81fc300352
Mailo Svetel <development@rooland.cz>
2015-04-05 20:13:27 +0200

    [upload] Moving file not creating

--- app/app.rb
+++ app/app.rb
@@ -7,11 +7,10 @@ module FUP
     end
 
     post '/upload' do
-      File.open('uploads/' + params['uploaded-file'][:filename], 'w') do |f|
-        f.write(params['uploaded-file'][:tempfile].read)
-      end
-      return 'The file was successfully uploaded!'
+      FileUtils.mv  params['uploaded-file'][:tempfile].path,
+                    'uploads/' + params['uploaded-file'][:filename]
 
+      return 'The file was successfully uploaded!'
     end
   end
 end
