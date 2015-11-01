c565cd6cf3f845ddff9c6b9ec08c31b23ae3c83e
Mailo Svetel <development@rooland.cz>
2015-11-01 13:58:47 +0100

    [cap][server] Now on Raj

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -7,7 +7,7 @@
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
-server 'yost.rlnd.cz', user: 'deploy', roles: %w{web app}, my_property: :my_value
+server 'raj.rlnd.cz', user: 'deploy', roles: %w{web app}, my_property: :my_value
 
 
 # role-based syntax
